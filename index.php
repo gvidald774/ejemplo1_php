dd23f883e623f6f8a54c05e6f582bd5e99b13d98
Guillermo Vidal Aldarias <gvidald774@g.educaand.es>
2021-10-04 08:51:54 +0200

    Primera versión

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>Hola buenas tardes</title>
+    </head>
+    <body>
+        <?php
+            $numero = rand(1,10);
+            switch($numero)
+            {
+                case 1:
+                    echo "<h1>Hola buenos días</h1>";
+                    break;
+                case 2:
+                    echo "<h2>Hola buenas tardes</h1>";
+                    break;
+                case 3:
+                    echo "<h3>Hola buenas noches</h3>";
+                    break;
+                case 4:
+                    echo "<h4>Hola que tal como estamos</h4>";
+                    break;
+                case 5:
+                    echo "<h5>Adiós</h5>";
+                    break;
+                default:
+                    echo "<h6>Aquí no hay nada que ver</h6>";
+                    break;
+            }
+        ?>
+    </body>
+</html>
